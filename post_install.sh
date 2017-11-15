a5b4b9cb9a6ce87c17ef26533848bb58089ce65e
Paul Ness <paul.ness@haymarketmedia.com>
2017-11-15 13:44:36 -0500

    Added a whole bunch of new software, tested on Ubuntu 16
    			39 "Rar"
    			40 "SmartGit"
    			41 "FireFox dev editi"
    			42 "Jekyll"
    			43 "Mouse scroller speed up to max"
    			44 "DupeGuru"
    			45 "Visual Studio Code Extensis"
    			46 "KeePass"
    			47 "Calibre"
    			48 "Set Ubuntu to local timeze"
    			49 "Update NPM"
    			50 "Android SDK and PheGap"
    			51 "VirtualBox"
    			52 "Samba GUI tools"
    			53 "OpenVPN"
    			54 "UGet download manager"
    			55 "FileZilla"
    			56 "PopcornTime"
    			57 "Robo3T"
    			58 "SQLite"
    			59 "SQLite Browser"
    			60 "Thunderbird"
    			61 "AdobeAir"
    			62 "IntelliJ"
    			63 "Steam"
    			64 "Peek"
    			65 "Stacer"
    			66 "OpenPics"
    			67 "Remmina"

--- post_install.sh
+++ post_install.sh
@@ -36,52 +36,81 @@ else
 
 	sudo apt-get install dialog
 	cmd=(dialog --separate-output --checklist "Please Select Software you want to install:" 22 76 16)
-	options=(1 "Sublime Text 3" off    # any option can be set to default to "on"
-	         2 "LAMP Stack" off
-	         3 "Build Essentials" off
-	         4 "Node.js" off
-	         5 "Git" off
-	         6 "Composer" off
-	         7 "JDK 9" off
-	         8 "Bleachbit" off
-	         9 "Ubuntu Restricted Extras" off
-	         10 "VLC Media Player" off
-	         11 "Unity Tewak Tool" off
-	         12 "Google Chrome" off
-	         13 "Teamiewer" off
-	         14 "Skype" off
-	         15 "Paper GTK Theme" off
-	         16 "Arch Theme" off
-	         17 "Arc Icons" off
-	         18 "Numix Icons" off
-			 19 "Multiload Indicator" off
-			 20 "Pensor" off
-			 21 "Netspeed Indicator" off
-			 22 "Generate SSH Keys" off
-			 23 "Ruby" off
-			 24 "Sass" off
-			 25 "Vnstat" off
-			 26 "Webpack" off
-			 27 "Grunt" off
-			 28 "Gulp" off
-		 29 "Atom" off
-		 30 ".Net Core 2.0.0 SDK" off
-		 31 "Visual Studio Code" off
-		 32 "Brackets" off
-		 33 "Gimps" off
-		 34 "Slack" off
-		 35 "Ubuntu Restricted Extras" on
-		 36 "Gnome Desktop" off
-		 37 "Yeoman & generators" off
-		 38 "Spotify" off)
+	options=(1 "Sublime Text 3" on    # any option can be set to "off"
+			2 "LAMP Stack" on
+			3 "Build Essentials" on
+			4 "Node.js" on
+			5 "Git" on
+			6 "Composer" on
+			7 "JDK 9" on
+			8 "Bleachbit" on
+			9 "Ubuntu Restricted Extras" on
+			10 "VLC Media Player" on
+			11 "Unity Tewak Tool" on
+			12 "Google Chrome" on
+			13 "Teamiewer" on
+			14 "Skype" on
+			15 "Paper GTK Theme" off
+			16 "Arch Theme" off
+			17 "Arc Icons" off
+			18 "Numix Icons" off
+			19 "Multiload Indicator" off
+			20 "Pensor" off
+			21 "Netspeed Indicator" off
+			22 "Generate SSH Keys" on
+			23 "Ruby" on
+			24 "Sass" on
+			25 "Vnstat" on
+			26 "Webpack" on
+			27 "Grunt" on
+			28 "Gulp" on
+			29 "Atom" on
+			30 ".Net Core 2.0.0 SDK" on
+			31 "Visual Studio Code" on
+			32 "Brackets" on
+			33 "Gimp" on
+			34 "Slack" on
+			35 "Ubuntu Restricted Extras" on
+			36 "Gnome Desktop" off
+			37 "Yeoman & generators" on
+			38 "Spotify" on
+			39 "Rar" on
+			40 "SmartGit" on
+			41 "FireFox dev edition" on
+			42 "Jekyll" on
+			43 "Mouse scroller speed up to max" on
+			44 "DupeGuru" on
+			45 "Visual Studio Code Extensions" on
+			46 "KeePass" on
+			47 "Calibre" on
+			48 "Set Ubuntu to local timezone" on
+			49 "Update NPM" on
+			50 "Android SDK and PhoneGap" on
+			51 "VirtualBox" on
+			52 "Samba GUI tools" on
+			53 "OpenVPN" on
+			54 "UGet download manager" on
+			55 "FileZilla" on
+			56 "PopcornTime" on
+			57 "Robo3T" on
+			58 "SQLite" on
+			59 "SQLite Browser" on
+			60 "Thunderbird" on
+			61 "AdobeAir" on
+			62 "IntelliJ" on
+			63 "Steam" on
+			64 "Peek" on
+			65 "Stacer" on
+			66 "OpenPics" on
+			67 "Remmina" on)
 
 		choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 		clear
 		for choice in $choices
 		do
 		    case $choice in
-	        	1)
-	            		#Install Sublime Text 3*
+			1)
+				#Install Sublime Text 3*
 				echo "Installing Sublime Text"
 				wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 				sudo apt-get install apt-transport-https
@@ -91,7 +120,7 @@ else
 				;;
 
 			2)
-			    	#Install LAMP stack
+				#Install LAMP stack
 				echo "Installing Apache"
 				apt install apache2 -y
 	            
@@ -331,6 +360,7 @@ else
 				echo "Installing Yeoman and generator webapp, aspnet, angular"
 				sudo npm install -g grunt-cli bower yo generator-karma generator-angular gulp-cli generator-angular-fullstack bower generator-webapp generator-wordpress generator-aspnet generator-generator	
 				;;
+
 			38) # Spotify
 				# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
 				sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
@@ -343,7 +373,224 @@ else
 
 				# 4. Install Spotify
 				sudo apt-get install spotify-client -y
+				;;
 
+			39) #Rar and other compressions libs
+				sudo apt-get install rar
+				sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
+				sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+				;;
+
+			40) #Smart GIT
+				sudo curl http://www.syntevo.com/smartgit/download?file=smartgit/smartgit-17_1_0.deb
+				sudo apt install smartgit-17_1_0.deb
+				;;
+
+			41) #FireFox developer edition
+				sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora
+				sudo apt-get update
+				sudo apt-get install firefox
+				;;
+
+			42) #Jekyll and all Ruby stuff
+				sudo apt-get install zlib1g-dev
+				sudo apt-get install ruby-full
+				sudo apt-get install rubygems
+				sudo bundle install
+				sudo gem install bundler
+				;;
+
+			43) #Speed up mouse scroller
+				sudo xinput set-prop 10 'Device Accel Constant Deceleration' 1 #scrolling on my mouse is weird/slow otherwise
+				;;
+
+			43) #DupeGuru (a great tool to find duplicate files)
+				sudo apt-add-repository ppa:hsoft/ppa
+				sudo apt-get update
+				sudo apt-get install dupeguru-se
+				;;
+			
+			44) #Hipchat
+				sudo sh -c 'echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-hipchat4.list'
+				sudo wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
+				sudo apt-get update
+				sudo apt-get install hipchat4
+				;;
+
+			45) #Visual Studio Code Extensions
+				code --install-extension streetsidesoftware.code-spell-checker
+				code --install-extension alexkrechik.cucumberautocomplete
+				code --install-extension glen-84.sass-lint
+				code --install-extension lkytal.coneelinter
+				code --install-extension ms-vscode.csharp
+				code --install-extension jchannon.csharpextensions
+				code --install-extension vsmobile.vscode-react-native
+				code --install-extension redhat.java
+				code --install-extension vscjava.vscode-java-debug
+				code --install-extension mdickin.markdown-shortcuts
+				code --install-extension msjsdiag.debugger-for-chrome
+				code --install-extension DavidAnson.vscode-markdownlint
+				code --install-extension ms-vscode.powershell
+				code --install-extension ms-vscode.node-debug2
+				code --install-extension ms-python.python
+				code --install-extension ms-vscode.cpptools
+				code --install-extension formulahendry.code-runner
+				code --install-extension dbaeumer.vscode-eslint
+				code --install-extension lukehoban.go
+				code --install-extension HookyQR.beautify
+				code --install-extension PeterJausovec.vscode-docker
+				code --install-extension eg2.vscode-npm-script
+				code --install-extension abusaidm.html-snippets
+				code --install-extension felixfbecker.php-intellisense
+				code --install-extension felixfbecker.php-debug
+				code --install-extension rebornix.ruby
+				code --install-extension ms-vscode.atom-keybindings
+				code --install-extension humao.rest-client
+
+
+				;;
+
+			46) #KeePass
+				sudo apt-add-repository ppa:jtaylor/keepass
+				sudo apt-get update
+				sudo apt-get install keepass2
+				;;
+
+			47) #Calibre
+				sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+				;;
+			
+			48) #Make Ubuntu use the same local timezone as Windows (as opposed to UTC) for dual booting only
+				sudo timedatectl set-local-rtc 1
+				;;
+
+			49) #Update NPM
+				sudo npm install npm@latest -g
+				;;
+			
+			50) #Install Android SDK and PhoneGap
+				sudo apt-add-repository ppa:cordova-ubuntu/ppa
+				sudo apt-get update
+				sudo apt-get install cordova-cli
+				sudo apt-get install ant
+				sudo npm install -g phonegap
+				;;
+
+			51) #VirtualBox and guest editions
+				sudo deb http://download.virtualbox.org/virtualbox/debian xenial contrib
+				wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+				wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+				sudo apt-get update
+				sudo apt-get install virtualbox
+				sudo apt upgrade
+
+				sudo apt-get update
+				sudo apt-get install virtualbox-guest-dkms 
+				;;
+
+			52) #Samba server
+				sudo add-apt-repository universe
+				sudo apt-get update
+				sudo apt-get install system-config-samba
+				sudo touch /etc/libuser.conf
+				;;
+
+			53) #OpenVPN
+			    sudo apt-get update
+   				sudo apt-get install openvpn easy-rsa
+				;;
+
+			54) #UGet (fast download manager)
+				sudo add-apt-repository ppa:plushuang-tw/uget-stable
+				sudo apt update
+				sudo apt install uget
+				;;
+
+			55) #FileZilla
+				sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu xenial-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
+				wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+				sudo apt update
+				sudo apt install filezilla
+				;;
+			
+			56) #PopcornTime
+				sudo ln -sf /lib/x86_64-linux-gnu/libudev.so.1 /lib/x86_64-linux-gnu/libudev.so.0
+				
+				wget https://dl.popcorn-time.to/Popcorn-Time-linux64.tar.gz
+				sudo mkdir /opt/popcorn-time
+				sudo tar -xzf Popcorn-Time-linux64.tar.gz -C /opt/popcorn-time
+				rm Popcorn-Time-linux64.tar.gz
+				
+				#to run PopcornTime add your own shortcut to /opt/popcorn-time/Popcorn-Time
+				;;
+
+			57) #Robo 3T (mongo viewer client)
+				wget https://download.robomongo.org/1.1.1/linux/robo3t-1.1.1-linux-x86_64-c93c6b0.tar.gz
+				sudo tar -xzf robo3t-1.1.1-linux-x86_64-c93c6b0.tar.gz -C /opt
+				rm robo3t-1.1.1-linux-x86_64-c93c6b0.tar.gz
+				sudo mkdir /opt/robo3t-1.1.1-linux-x86_64-c93c6b0//lib/BKP/
+				sudo mv /opt/robo3t-1.1.1-linux-x86_64-c93c6b0/lib/libstdc++* /opt/robo3t-1.1.1-linux-x86_64-c93c6b0//lib/BKP/
+				sudo ln -s /opt/robo3t-1.1.1-linux-x86_64-c93c6b0/bin/robo3t /usr/bin/robo3t
+				;;
+
+			58) #SQLite
+				sudo add-apt-repository ppa:jonathonf/backports
+				sudo apt update
+				sudo apt-get install sqlite3
+				;;
+
+			59) #SQLite browser
+				sudo add-apt-repository -y ppa:linuxgndu/sqlitebrowser
+				sudo apt-get update
+				sudo apt-get install sqlitebrowser
+				;;
+			
+			60) #Thunderbird
+				sudo add-apt-repository ppa:ubuntu-mozilla-security/ppa
+				sudo apt-get update
+				sudo apt-get install thunderbird
+				;;
+
+			61) #Adobe Air
+				wget -O adobe-air.sh http://drive.noobslab.com/data/apps/AdobeAir/adobe-air.sh
+				chmod +x adobe-air.sh;sudo ./adobe-air.sh
+				;;
+
+			62) #IntelliJ IDE Community Edition for JAVA
+				sudo add-apt-repository ppa:ubuntuhandbook1/apps
+				sudo apt-get update
+				sudo apt-get install intellij-idea-community
+				;;
+				
+			63) #Steam Gaming
+				sudo add-apt-repository multiverse
+				sudo apt-get update
+				sudo apt-get install steam
+				;;
+
+			64) #Peek (screen capture to animated GIF's / demo bugs and stuff)
+				sudo add-apt-repository ppa:peek-developers/stable
+				sudo apt-get update
+				sudo apt-get install peek
+				;;
+
+			65) #Stacer (system resource monitor etc)
+				wget https://github.com/oguzhaninan/Stacer/releases/download/v1.0.8/stacer_1.0.8_amd64.deb
+				sudo dpkg --install stacer_1.0.8_amd64.deb
+				sudo rm -rf stacer_1.0.8_amd64.deb
+				;;
+
+			66) #OpenPics (open source images to use with any web project or template etc)
+				wget https://github.com/lohanitech/openpics/releases/download/v0.2.0/openpics_0.2.0_amd64.deb
+				sudo dpkg --install openpics_0.2.0_amd64.deb
+				sudo rm -rf openpics_0.2.0_amd64.deb
+				;;
+
+			67) #Remmina (remote desktop client)
+				sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
+				sudo apt-get update
+				sudo apt-get install remmina remmina-plugin-rdp remmina-plugin-gnome libfreerdp-plugins-standard
+				;;
 	    esac
 	done
 fi
