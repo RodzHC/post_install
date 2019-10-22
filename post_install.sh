33918db28cd92dbdeac55a7ed17dbcafd1721851
Rodrigo Marzullo <rodrigo.marzullo@gmail.com>
2019-10-22 11:45:05 -0300

    Fix s

--- post_install.sh
+++ post_install.sh
@@ -1,63 +1,57 @@
 #!/bin/bash
 
-curl_check ()
-{
-  echo "Checking for curl..."
-  if command -v curl > /dev/null; then
-    echo "Detected curl..."
-  else
-    echo "Installing curl..."
-    apt-get install -q -y curl
-  fi
+curl_check() {
+	echo "Checking for curl..."
+	if command -v curl >/dev/null; then
+		echo "Detected curl..."
+	else
+		echo "Installing curl..."
+		apt-get install -q -y curl
+	fi
 }
 
-node_check ()
-{
-  echo "Checking for Nodejs..."
-  if command -v node > /dev/null; then
-    echo "Detected Nodejs..."
-  else
-    echo "Installing Nodejs"
-    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-    apt install -y nodejs
-  fi
+node_check() {
+	echo "Checking for Nodejs..."
+	if command -v node >/dev/null; then
+		echo "Detected Nodejs..."
+	else
+		echo "Installing Nodejs"
+		curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+		apt install -y nodejs
+	fi
 }
 
-make_dialog ()
-{
+make_dialog() {
 
 	cmd=(dialog --separate-output --checklist $box_message 22 76 16)
 	choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 	all_choices=(${choices[@]} ${all_choices[@]})
 
-
 }
 
 if [[ $EUID -ne 0 ]]; then
-   	echo "This script must be run as root" 
-   	exit 1
+	echo "This script must be run as root"
+	exit 1
 else
 	#Update and Upgrade
 	echo "Updating and Upgrading"
 	apt-get update && sudo apt-get upgrade -y
 
-	
 	#Check if curl is installed
 	curl_check
 
 	sudo apt-get install dialog
-	
+
 	box_message="DEV"
-	option=(
+	options=(
 		1 "Node.js" on
 		2 "Python" off #Ubuntu 18.4 comes with python 3.6
 		3 "Build Essentials" off
 	)
 	make_dialog
 
-
 	box_message="DEVOPs"
-	option=(
+	options=(
 		30 "Git" on
 		31 "Dockers" on
 
@@ -65,8 +59,8 @@ else
 	make_dialog
 
 	box_message="Text editors and IDEs"
-	option=(
-		101 "Sublime Text 3" on  
+	options=(
+		101 "Sublime Text 3" on
 		102 "Atom" on
 		103 "Visual Studio Code" on
 		104 "Brackets" on
@@ -81,7 +75,7 @@ else
 	make_dialog
 
 	box_message="Utilities"
-	option=(
+	options=(
 		201 "VLC Media Player" on
 		202 "Skype" on
 		204 "Google Chrome" on
@@ -95,8 +89,7 @@ else
 	make_dialog
 
 	clear
-	for choice in $all_choices
-	do
+	for choice in $all_choices; do
 		case $choice in
 		1)
 			#Install Nodejs
@@ -111,12 +104,11 @@ else
 			sudo apt-get update
 			sudo apt-get install python3.6
 			;;
-		3)	
+		3)
 			#Install Build Essentials
 			echo "Installing Build Essentials"
 			apt install -y build-essential
 			;;
-			
 
 		30)
 			#Install git
@@ -128,24 +120,26 @@ else
 			sudo apt-get remove docker docker-engine docker.io containerd runc
 			sudo apt-get update
 			sudo apt-get install \
-    		apt-transport-https \
-    		ca-certificates \
-   			curl \
-    		gnupg-agent \
-   			software-properties-common
+				apt-transport-https \
+				ca-certificates \
+				curl \
+				gnupg-agent \
+				software-properties-common
 
 			curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 			sudo apt-key fingerprint 0EBFCD88
 			sudo add-apt-repository \
-  			"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+				"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    			$(lsb_release -cs) \
    			stable"
 
 			sudo apt-get update
 
 			sudo apt-get install docker-ce docker-ce-cli containerd.iosudo apt-get install docker-ce docker-ce-cli containerd.io
+			;;
 
 		101)
+
 			#Install Sublime Text 3*
 			echo "Installing Sublime Text"
 			wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -154,47 +148,48 @@ else
 			sudo apt-get update
 			sudo apt-get install sublime-text -y
 			;;
-		102)	
+		102)
 			echo "Installing Atom"
 			wget https://atom.io/download/deb -O atom.deb
 			sudo dpkg -i atom.deb
 			# Install Atom's dependencies if they are missing
 			sudo apt-get -f install -y
-			rm -f atom.deb	
+			rm -f atom.deb
 			;;
-		103)	echo "Installing Visual Studio Code"
-			curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+		103)
+			echo "Installing Visual Studio Code"
+			curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
 			sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 			sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 			sudo apt update
 			sudo apt install code -y
 			;;
-		104)	echo "Installing Brackets"
+		104)
+			echo "Installing Brackets"
 			sudo add-apt-repository ppa:webupd8team/brackets -y
 			sudo apt-get update
 			sudo apt-get install brackets -y
 			;;
-		105)
-
-			;;
+		105) ;;
 
 		151)
+
 			#Install LAMP stack
 			echo "Installing Apache"
 			apt install apache2 -y
-			
+
 			echo "Installing Mysql Server"
 			apt install mysql-server -y
 
 			echo "Installing PHP"
 			apt install php libapache2-mod-php php-mcrypt php-mysql -y
-			
+
 			echo "Installing Phpmyadmin"
 			apt install phpmyadmin -y
 
 			echo "Cofiguring apache to run Phpmyadmin"
-			echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
-			
+			echo "Include /etc/phpmyadmin/apache.conf" >>/etc/apache2/apache2.conf
+
 			echo "Enabling module rewrite"
 			sudo a2enmod rewrite
 			echo "Restarting Apache Server"
@@ -211,7 +206,7 @@ else
 			apt install apt-transport-https -y
 			curl https://repo.skype.com/data/SKYPE-GPG-KEY | apt-key add -
 			echo "deb https://repo.skype.com/deb stable main" | tee /etc/apt/sources.list.d/skypeforlinux.list
-			apt update 
+			apt update
 			apt install skypeforlinux -y
 			;;
 		204)
@@ -220,7 +215,7 @@ else
 			echo "Installing Google Chrome"
 			wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 			sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-			apt-get update 
+			apt-get update
 			apt-get install google-chrome-stable -y
 			;;
 		205)
@@ -232,7 +227,7 @@ else
 			rm -rf teamviewer_i386.deb
 			;;
 
-		206) 
+		206)
 			# Spotify
 			# 1. Add the Spotify repository signing keys to be able to verify downloaded packages
 			sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
@@ -247,20 +242,18 @@ else
 			sudo apt-get install spotify-client -y
 			;;
 
-
-		207)	
+		207)
 			echo "Installing Gimp"
 			sudo apt-get install gimp -y
 			;;
 
-		208) 	
+		208)
 			echo "Installing Slack"
 			wget -O slack-desktop.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-2.8.2-amd64.deb
 			sudo dpkg -i slack-desktop.deb
 			sudo apt-get install -f -y
 			rm -f slack-desktop.deb
 			;;
-
 
 		209) #VirtualBox and guest editions
 			sudo deb http://download.virtualbox.org/virtualbox/debian xenial contrib
@@ -271,9 +264,9 @@ else
 			sudo apt upgrade
 
 			sudo apt-get update
-			sudo apt-get install virtualbox-guest-dkms 
+			sudo apt-get install virtualbox-guest-dkms
 			;;
 
-	esac
-done
+		esac
+	done
 fi
